0719e7696df4c5fe5ef919897e3308caf4cfa50a
Kevin-Jakiel <kjakiel@ucsd.edu>
2022-04-28 22:43:48 -0700

    Update SkillDemoTester.java

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -2,10 +2,10 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoTester {
-    
+
     @Test
     public void square() {
-        assertEquals("The square of 13", 38, SkillDemo.square(13));
+        assertEquals("The square of 13", 169, SkillDemo.square(13));
     }
  
 }
